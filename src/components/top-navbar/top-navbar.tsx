e35ad79316bda042de96be58b1395ce93c68ebef
Sebastian Burman <74584954+sebastianjburman@users.noreply.github.com>
2023-05-31 18:28:51 -0400

    Navbar sticky

--- src/components/top-navbar/top-navbar.tsx
+++ src/components/top-navbar/top-navbar.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "flowbite-react";
 
 export default function TopNavbar() {
   return (
-    <div className=" bg-zinc-800">
+    <div className="bg-zinc-800 sticky top-0">
       <Navbar fluid className="bg-zinc-800 max-w-screen-xl mx-auto  ">
         <Navbar.Brand href="/">
           <img
